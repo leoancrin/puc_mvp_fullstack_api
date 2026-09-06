0cbd4f872998fa76d841f9ae9c1d85d0c78548ec
Leonardo A. <leonardoancrin@gmail.com>
2026-09-06 17:58:34 -0300

    Altera response das rotas no app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from extensions import db
 
 from metodos.metodos_tipo_item import *
 
+from schemas.tipos_item_schema import *
+from schemas.error import *
+
+from requisicao.tipo_item_requests import *
+
 info = Info(title='API de itens colecionáveis', version='1.0.0')
 app = OpenAPI(__name__, info=info)
 
@@ -27,20 +32,52 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 # Rotas relacionadas aos tipos de itens
-@app.post('/cadastrar/tipos', tags=[tipo_item_tag])
-def cadastrar_tipos():
+@app.post('/cadastrar/tipos', tags=[tipo_item_tag], 
+          responses={
+              200: TipoItemSchema,
+              400: ErroSchema,
+              409: ErroSchema
+          })
+def cadastrar_tipos(body: TipoItemBodySomenteTipoItem):
+    """
+        Cadastra um tipo de item colecionável
+    """
     return metodo_cadastrar_tipos()
 
-@app.get('/consultar/tipos', tags=[tipo_item_tag])
+@app.get('/consultar/tipos', tags=[tipo_item_tag],
+         responses={
+             200: ListTipoItemSchema,
+             400: ErroSchema
+         })
 def consultar_tipos():
+    """
+        Consulta todos os tipo de item colecionável cadastrados
+    """
     return metodo_consultar_tipos()
 
-@app.put('/alterar/tipos', tags=[tipo_item_tag])
-def alterar_tipos():
+@app.put('/alterar/tipos', tags=[tipo_item_tag],
+         responses={
+             200: AtualizaTipoItemSchema,
+             400: ErroSchema,
+             422: ErroSchema
+         })
+def alterar_tipos(body: TipoItemBodyCompleto):
+    """
+        Altera um tipo de item colecionável cadastrado anteriormente
+    """
     return metodo_alterar_tipos() 
 
-@app.delete('/deletar/tipos', tags=[tipo_item_tag])
-def deletar_tipos():
+@app.delete('/deletar/tipos', tags=[tipo_item_tag],
+            responses={
+                200: DeletaTipoItemSchema,
+                400: ErroSchema,
+                404: ErroSchema,
+                422: ErroSchema
+            })
+def deletar_tipos(body: TipoItemBodyCompleto):
+    """
+        Deleta um tipo de item colecionável existente
+    """
     return metodo_deletar_tipos() 
 
 # Rotas relacionadas aos itens 
