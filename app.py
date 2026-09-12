390e19eefd2fc3230e81a18186668cdb1a0b4983
Leonardo A. <leonardoancrin@gmail.com>
2026-09-12 15:08:01 -0300

    Alterado app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask_openapi3 import Info, OpenAPI, Tag
 from extensions import db
 
 from metodos.metodos_tipo_item import *
+from metodos.metodos_item_colecionavel import *
 
 from schemas.tipos_item_schema import *
+from schemas.itens_colecionaveis_schema import *
 from schemas.error import *
 
 from requisicao.tipo_item_requests import *
+from requisicao.itens_colecionaveis_request import *
 
 info = Info(title='API de itens colecionáveis', version='1.0.0')
 app = OpenAPI(__name__, info=info)
@@ -37,7 +40,7 @@ def hello_world():
               200: TipoItemSchema,
               400: ErroSchema,
               409: ErroSchema
-          })
+            })
 def cadastrar_tipos(body: TipoItemBodySomenteTipoItem):
     """
         Cadastra um tipo de item colecionável
@@ -48,7 +51,7 @@ def cadastrar_tipos(body: TipoItemBodySomenteTipoItem):
          responses={
              200: ListTipoItemSchema,
              400: ErroSchema
-         })
+            })
 def consultar_tipos():
     """
         Consulta todos os tipos de itens cadastrados
@@ -58,9 +61,8 @@ def consultar_tipos():
 @app.put('/alterar/tipos', tags=[tipo_item_tag],
          responses={
              200: AtualizaTipoItemSchema,
-             400: ErroSchema,
-             422: ErroSchema
-         })
+             400: ErroSchema
+            })
 def alterar_tipos(body: TipoItemBodyCompleto):
     """
         Altera um tipo de item cadastrado anteriormente
@@ -81,30 +83,61 @@ def deletar_tipos(body: TipoItemBodyCompleto):
     return metodo_deletar_tipos() 
 
 # Rotas relacionadas aos itens 
-@app.post('/cadastrar/itens', tags=[item_colecionavel_tag])
-def cadastrar_itens():
+@app.post('/cadastrar/itens', tags=[item_colecionavel_tag],
+          responses={
+              200: CadastraItemColecionavelSchema,
+              400: ErroSchema,
+              409: ErroSchema
+            })
+def cadastrar_itens(body: ItemColecionavelBodyCadastro):
     """
         Cadastra um item colecionável
     """
-    return "<p>Cadastrar itens ainda não implementado!</p>"
+    return metodo_cadastrar_item()
 
-@app.get('/consultar/itens', tags=[item_colecionavel_tag])
+@app.get('/consultar/itens', tags=[item_colecionavel_tag],
+         responses={
+             200: ListItemColecionavelSchema,
+             400: ErroSchema
+         })
+
 def consultar_itens():
     """
         Consulta todos os item colecionáveis existentes
     """
-    return "<p>Consultar itens ainda não implementado!</p>"
+    return metodo_consultar_itens_colecionaveis()
 
-@app.put('/alterar/itens', tags=[item_colecionavel_tag])
-def alterar_itens():
+@app.get('/consultar/itens/<string:tipo_item_colecionavel>', tags=[item_colecionavel_tag],
+         responses={
+             200: ListItemColecionavelSchema,
+             400: ErroSchema
+            })
+def consultar_itens_por_tipo(path: ItemColecionavelPorTipoPath):
+    """
+        Consulta todos os item colecionáveis existentes para um tipo de item específico
+    """
+    return metodo_consultar_itens_colecionaveis_por_tipo(path)
+
+@app.put('/alterar/itens', tags=[item_colecionavel_tag],
+         responses={
+             200: AtualizaItemColecionavelSchema,
+             400: ErroSchema,
+             409: ErroSchema
+         })
+def alterar_itens(body: ItemColecionavelBodyCompleto):
     """
         Altera um item colecionável cadastrado anteriormente
     """
-    return "<p>Alterar itens ainda não implementado!</p>"
+    return metodo_alterar_item_colecionável()
 
-@app.delete('/deletar/itens', tags=[item_colecionavel_tag])
-def deletar_itens():
+@app.delete('/deletar/itens', tags=[item_colecionavel_tag],
+            responses={
+                200: DeletaItemColecionavelSchema,
+                400: ErroSchema,
+                409: ErroSchema
+            })
+def deletar_itens(body: ItemColecionavelBodyCompleto):
     """
         Deleta item colecionável cadastrado
     """
-    return "<p>Deletar itens ainda não implementado!</p>"
+    return metodo_deletar_item_colecionável()
