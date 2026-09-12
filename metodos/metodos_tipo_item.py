898e410ea6fc8ccdd60695ea3071e46c3054145f
Leonardo A. <leonardoancrin@gmail.com>
2026-09-12 15:06:48 -0300

    Alterado metodos/metodos_tipo_item

--- metodos/metodos_tipo_item.py
+++ metodos/metodos_tipo_item.py
@@ -5,20 +5,23 @@ from modelos.tipos_item import TipoItemColecionavel
 from extensions import db
 
 # Funções gerais
-def valida_tipo_texto(texto):
+def valida_requisicao(texto, id=1):
     if not texto:
         return jsonify({"Erro":"Tipo é obrigatório"}), 400
+    if id < 1:
+        print(id)
+        return jsonify({"Erro":"ID com valor inválido"}), 400
 
 def metodo_cadastrar_tipos():
     dados = request.get_json()
 
     tipo_requisicao = dados.get("tipo_item")
 
-    valida_tipo_texto(tipo_requisicao) 
+    valida_requisicao(tipo_requisicao) 
 
     try:
         tipo_cadastrado = TipoItemColecionavel(
-            tipo_item = dados.get("tipo_item")
+            tipo_item = tipo_requisicao
         )
 
         select_tipo_pelo_nome = db.select(TipoItemColecionavel).filter_by(tipo_item=tipo_requisicao)
@@ -30,24 +33,29 @@ def metodo_cadastrar_tipos():
         db.session.add(tipo_cadastrado)
         db.session.commit()
 
-        return jsonify({"mensagem":"Tipo cadastrado com sucesso"}), 200
+        resposta = [{"tipo_item":tipo_requisicao,
+                     "mensagem": "Tipo cadastrado com sucesso"
+                     }]
+
+        return jsonify(resposta), 200
     except Exception as e:
         db.session.rollback()
-        return jsonify({"Erro ao cadastrar tipo": str(e)}), 400
+        return jsonify({"Erro": "Erro nao identificado:" + str(e)}), 400
 
 def metodo_consultar_tipos():
     try:
         select_todos_tipos = db.select(TipoItemColecionavel).order_by(TipoItemColecionavel.id_tipo)
         tipos_itens = db.session.execute(select_todos_tipos).scalars().all()
 
-        resultado = [
+        resposta = [
             {"id_tipo": tipo.id_tipo, "tipo_item": tipo.tipo_item}
             for tipo in tipos_itens
         ]
-        return jsonify(resultado), 200
+
+        return jsonify(resposta), 200
     
     except Exception as e:
-        return jsonify({"Erro ao recuperar os tipos": str(e)}), 400
+        return jsonify({"Erro": "Erro nao identificado:" + str(e)}), 400
 
 def metodo_alterar_tipos():
     dados = request.get_json()
@@ -55,24 +63,29 @@ def metodo_alterar_tipos():
     tipo_requisicao = dados.get("tipo_item")
     id_requisicao = dados.get("id_tipo")
    
-    valida_tipo_texto(tipo_requisicao) 
+    valida_requisicao(tipo_requisicao, id_requisicao) 
    
     try:   
         select_tipo_por_id = db.select(TipoItemColecionavel).filter_by(id_tipo=id_requisicao)   
         tipo_a_alterar = db.session.execute(select_tipo_por_id).scalar_one_or_none()
 
         if tipo_a_alterar is None:
-            return jsonify({"mensagem":"Tipo não existe para ser alterado"}), 404
+            return jsonify({"Erro":"Tipo não existe para ser alterado"}), 404
 
         tipo_a_alterar.tipo_item = tipo_requisicao
            
         db.session.commit()
+
+        resposta = [{"id_tipo": id_requisicao,
+                     "tipo_item":tipo_requisicao,
+                     "mensagem":"Tipo alterado com sucesso"
+                     }]
    
-        return jsonify({"mensagem":"Tipo alterado com sucesso"}), 200
+        return jsonify(resposta), 200
     
     except Exception as e:
         db.session.rollback()
-        return jsonify({"Erro ao alterar tipo": str(e)}), 400 
+        return jsonify({"Erro": "Erro nao identificado:" + str(e)}), 400 
 
 def metodo_deletar_tipos():
     dados = request.get_json()
@@ -80,30 +93,35 @@ def metodo_deletar_tipos():
     tipo_requisicao = dados.get("tipo_item")
     id_requisicao = dados.get("id_tipo")
       
-    valida_tipo_texto(tipo_requisicao)
+    valida_requisicao(tipo_requisicao, id_requisicao)
 
     try:
         select_tipo_a_ser_deletado = db.select(TipoItemColecionavel).filter_by(id_tipo=id_requisicao)
         tipo_a_ser_deletado = db.session.execute(select_tipo_a_ser_deletado).scalar_one_or_none()
 
         if tipo_a_ser_deletado is None:
-            return jsonify({"Erro ao deletar tipo": "Tipo inexistente"}), 404
+            return jsonify({"Erro": "Tipo inexistente"}), 404
 
         if tipo_a_ser_deletado.tipo_item != tipo_requisicao:
-            return jsonify({"Erro ao deletar tipo": "Verifique se o nome do tipo está correto"}), 409
+            return jsonify({"Erro": "Verifique se o nome do tipo está correto"}), 409
 
         select_item_colecionavel = db.select(ItensColecionaveis).filter_by(tipo=id_requisicao)
         tipo_existente_no_item = db.session.execute(select_item_colecionavel).scalar_one_or_none()
 
         if tipo_existente_no_item:
-            return jsonify({"Erro ao deletar tipo": "Existe item com esse tipo definido"}), 409
+            return jsonify({"Erro": "Não permitido deletar tipo com item cadastrado "}), 409
 
 
         db.session.delete(tipo_a_ser_deletado) 
         db.session.commit()
 
-        return jsonify({"mensagem": "Tipo deletado com sucesso"}), 200 
+        resposta = [{"id_tipo": id_requisicao,
+                     "tipo_item":tipo_requisicao,
+                     "mensagem":"Tipo deletado com sucesso"
+                     }]
+
+        return jsonify(resposta), 200 
 
     except Exception as e:
         db.session.rollback()
-        return jsonify({"Erro ao deletar tipo": str(e)}), 400 
+        return jsonify({"Erro": "Erro nao identificado:" + str(e)}), 400 
