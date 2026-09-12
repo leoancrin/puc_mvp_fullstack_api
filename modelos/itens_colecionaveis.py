c421f4971036e090260879386514aad850657b3b
Leonardo A. <leonardoancrin@gmail.com>
2026-09-12 15:07:10 -0300

    Alterado modelos/itens_colecionaveis

--- modelos/itens_colecionaveis.py
+++ modelos/itens_colecionaveis.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Integer, String, ForeignKey, Float
+from sqlalchemy import Integer, String, ForeignKey, Numeric
 from sqlalchemy.orm import Mapped, mapped_column
 from extensions import db
+from decimal import Decimal
 
 class ItensColecionaveis(db.Model):
     __tablename__ = 'itens_colecionaveis'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id_item: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     tipo = mapped_column(ForeignKey("tipo_item_colecionavel.id_tipo"))
     nome_item: Mapped[str] = mapped_column(String(50), nullable=False, default="Nome não definido")
-    valor_item: Mapped[float] = mapped_column(Float, nullable=False, default=0.0)
+    valor_item: Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2), nullable=False, default=0.00)
 
