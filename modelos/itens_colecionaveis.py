2b823a56f65dbd2f75fc92ffa1fb8d9d6fa2ad0e
Leonardo A. <leonardoancrin@gmail.com>
2026-09-25 23:25:00 -0300

    Altera modelos/itens_colecionaveis

--- modelos/itens_colecionaveis.py
+++ modelos/itens_colecionaveis.py
@@ -7,7 +7,7 @@ class ItensColecionaveis(db.Model):
     __tablename__ = 'itens_colecionaveis'
 
     id_item: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    tipo = mapped_column(ForeignKey("tipo_item_colecionavel.id_tipo"))
+    tipo: Mapped[int] = mapped_column(ForeignKey("tipo_item_colecionavel.id_tipo"))
     nome_item: Mapped[str] = mapped_column(String(50), nullable=False, default="Nome não definido")
     valor_item: Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2), nullable=False, default=0.00)
 
